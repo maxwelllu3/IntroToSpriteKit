dcfac04a4cde45f4bc5939c7c286972a2bd490ff
Russell Gordon <rgordon@lcs.on.ca>
2019-12-07 09:41:38 -0500

    Got initial page written to introduce what SpriteKit is.

---
+++ IntroToSpriteKit.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+ # Intro to SpriteKit
+ 
+ UIKit is a collection of functionality that Apple provides software developers so that they can author iOS apps more easily.
+ 
+ UIKit offers classes such as UITextField, UIButton, and UILabel, which we've made use of in prior tasks.  UIKit offers more advanced classes like UITableView, which are very common in iOS apps. For example:
+ 
+ ![UITableView](UITableViewExamples.png "Some examples of UITableView in various iOS apps.")
+ 
+ SpriteKit is a collection of functionality that Apple provides software developers so that they can more easily author a different type of app: an animation, or a game.
+ 
+ Page 1 | [Next: Creating a SpriteKit Scene](@next)
+ */

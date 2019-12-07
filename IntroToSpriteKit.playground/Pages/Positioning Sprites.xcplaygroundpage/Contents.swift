2623e096a31925e46ce5a5ee3a207ad1b44d15fb
Russell Gordon <rgordon@lcs.on.ca>
2019-12-07 11:10:58 -0500

    Positioning sprites page now completed.

--- IntroToSpriteKit.playground/Pages/Positioning Sprites.xcplaygroundpage/Contents.swift
+++ IntroToSpriteKit.playground/Pages/Positioning Sprites.xcplaygroundpage/Contents.swift
@@ -29,7 +29,7 @@ PlaygroundSupport.PlaygroundPage.current.liveView = view
  
  More commonly, sprites are created from the `SKSpriteNode` class and based on a bitmap image you provide.
  
- This means sprites in your games or animations can be images you find online, or images you create yourself (for example, using an iPad, or by scanning hand-drawn images).
+ This means that sprites in your games or animations can be images you find online, or images you create yourself (for example, using an iPad, or by scanning hand-drawn images).
  
  - Callout(Explore):
  Click the disclosure triangle for this playground page. Do the same for the **Resources** subfolder. What do you see?
@@ -56,11 +56,71 @@ circle.position = CGPoint(x: 0, y: 0)
  
     What does the position of the sprite tell you about the location of the origin in a SpriteKit scene?
  
+    What does the appearance of the circle tell you about where a sprite is "anchored"?
+ 
  */
 
 //scene.addChild(circle)
- 
+
 /*:
+ 
+ You can add many sprites based on the same image file.
+ 
+ - Experiment:
+    Uncomment the blocks of code below, one block at a time. Run the playground after uncommenting each block.
+ 
+Remember that you can select a block of code by positioning your cursor at the left edge of the first line, then hold down the **Shift** key, then press the **↓** key.
+ 
+Once you have selected the block of code, press the **Command** and / keys at the same time to toggle the comments off all at once.
+
+ */
+
+//// Add another circle to the right of the origin
+//let circle2 = SKSpriteNode(imageNamed: "circle")
+//circle2.position = CGPoint(x: 100, y: 0)
+//scene.addChild(circle2)
+
+//// Add another circle further above the origin
+//let circle3 = SKSpriteNode(imageNamed: "circle")
+//circle3.position = CGPoint(x: 0, y: 200)
+//scene.addChild(circle3)
+
+//// Add a circle at the bottom right corner of the scene
+//let circle4 = SKSpriteNode(imageNamed: "circle")
+//circle4.position = CGPoint(x: scene.size.width, y: 0)
+//scene.addChild(circle4)
+
+//// Add a star at the right side of the scene, halfway up
+//let star = SKSpriteNode(imageNamed: "star")
+//star.position = CGPoint(x: scene.size.width, y: scene.size.height / 2)
+//scene.addChild(star)
+
+/*:
+ - Callout(Reflect):
+     Does the co-ordinate system for a SpriteKit scene work like you expected?
+ 
+ ### Exercises
+ 
+ Write code to complete each of the following tasks.
+ 
+ 1. Add a star to the scene, centred vertically and horizontally.
+ 2. Add a circle to the top right corner of the scene.
+ 3. Add a square to the scene, centred horizontally, and at the very top of the scene.
+ 
+ */
+
+// Exercise 1: Write your code below.
+
+
+// Exercise 2: Write your code below.
+
+
+// Exercise 3: Write your code below.
+
+
+
+/*:
+
  [Previous: Creating a SpriteKit Scene](@previous) | Page 3 | [Next: ???](@next)
  
  

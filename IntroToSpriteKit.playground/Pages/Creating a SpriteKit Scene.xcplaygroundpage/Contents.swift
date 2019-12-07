14f0b960927b7904729cf18a26df2dfb562b4da0
Russell Gordon <rgordon@lcs.on.ca>
2019-12-07 10:09:51 -0500

    Scene intro page completed

---
+++ IntroToSpriteKit.playground/Pages/Creating a SpriteKit Scene.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+/*:
+ ## Creating a SpriteKit Scene
+ 
+ Everything that appears on screen in a SpriteKit-based app is a *node*.
+ 
+ The *scene* is the *parent node*.
+ 
+ Let's create and present a scene now.
+ 
+ ### Required Frameworks
+ 
+ First we must important some required frameworks.
+ */
+import PlaygroundSupport    // Allows playground to show live view
+import SpriteKit            // Provides 2D graphics classes
+
+/*:
+ ### Create the scene
+ */
+// Define a frame with the desired size (an instance of the CGRect class)
+let frame = CGRect(x: 0, y: 0, width: 400, height: 320)
+
+// Define a scene to contain our game or animation (an instance of the SKScene class)
+var scene = SKScene(size: frame.size)
+
+/*:
+ ### Present the scene
+ In projects built upon the UIKit framework, you have used Interface Builder to add views to a storyboard.
+ 
+ Here, we create a view using code instead, and then show that view in the playground's live view area.
+ */
+
+// Define a view (an instance of SKView)
+let view = SKView(frame: frame)
+
+// Ask the view to show how many nodes exist in the scene
+view.showsNodeCount = true
+
+// Present the scene we just configured
+view.presentScene(scene)
+
+// Show the view in the live view area
+PlaygroundSupport.PlaygroundPage.current.liveView = view
+
+/*:
+ ### Node count
+ 
+ Run the playground.
+ 
+ Notice that the node count is 1. That node is the scene itself.
+ 
+[Previous: UIKit vs. SpriteKit](@previous) | Page 2 | [Next: Creating a SpriteKit Scene](@next)
+*/
---
+++ IntroToSpriteKit.playground/Pages/UIKit vs SpriteKit.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+ # Intro to SpriteKit
+ 
+ ## UIKit vs. SpriteKit
+ 
+ UIKit is a *framework* (a collection of functionality) that Apple provides software developers so that they can author iOS apps more easily.
+ 
+ UIKit offers classes such as UITextField, UIButton, and UILabel, which we've made use of in prior tasks.  UIKit also offers more advanced classes like UITableView – commonly used in iOS apps. Some examples:
+ 
+ ![UITableView](UITableViewExamples.png "Some examples of UITableView in various iOS apps.")
+*Image from [Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uitableview).*
+ 
+ [SpriteKit](https://developer.apple.com/documentation/spritekit) is another framework that Apple provides to software developers so that they can more easily author a different type of app: an animation, or a game.
+ 
+ Page 1 | [Next: Creating a SpriteKit Scene](@next)
+ */

6f3da82c111ec1e0e290b1f147e2ef0908c1ca19
Russell Gordon <rgordon@lcs.on.ca>
2019-12-07 09:18:54 -0500

    Initial Commit

---
+++ IntroToSpriteKit/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  IntroToSpriteKit
+//
+//  Created by Russell Gordon on 2019-12-07.
+//  Copyright © 2019 Russell Gordon. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}

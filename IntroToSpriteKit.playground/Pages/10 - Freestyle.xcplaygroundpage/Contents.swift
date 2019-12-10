ea7b99751de970077cc2ba605b5a5abc3a660789
Russell Gordon <github@russellgordon.ca>
2019-12-10 15:00:12 -0500

    Added sprite resources links to Freestyle page.

--- IntroToSpriteKit.playground/Pages/10 - Freestyle.xcplaygroundpage/Contents.swift
+++ IntroToSpriteKit.playground/Pages/10 - Freestyle.xcplaygroundpage/Contents.swift
@@ -24,6 +24,32 @@ view.presentScene(scene)
 PlaygroundSupport.PlaygroundPage.current.liveView = view
 
 /*:
+ ### Useful Resources
+ 
+ #### Sprites
+ 
+ The [Sprite Database](http://spritedatabase.net/) has spritesheets for a wide variety of classic video games.
+ 
+ Of course, you can also draw sprites yourself (using an iPad, or by hand-drawing images and scanning or photographing them).
+ 
+ Note that [using copyrighted assets is permissable for personal use in Canada](https://en.wikipedia.org/wiki/Fair_dealing_in_Canadian_copyright_law), but if you wish to publish your work in any fashion – you need to find image assets with [compatible licensing terms](https://search.creativecommons.org).
+ 
+ [Open Game Art](https://opengameart.org) is another good option for graphics assets for your games or animations.
+ 
+ #### Decompiling Sprite Sheets
+ 
+ To use arrays of images [like we did on page 8](8%20-%20Animating%20a%20Sprite) you'd get very bored very quickly slicing up a spritesheet manually.
+ 
+ Programs exist that do this automatically for you.
+ 
+ 1. Download and [install Adobe Air](https://get.adobe.com/air/).
+ 2. Download and [install Shoebox](http://renderhjs.net/shoebox/).
+ 
+ After those programs are installed, you can [use a sprite sheet like this one](http://spritedatabase.net/files/snes/541/Sprite/MarioLuigi.gif), and [feed it into Shoebox like this](http://renderhjs.net/shoebox/extractSprites.htm) to extract the individual images.
+ 
+ */
+
+/*:
 
  [Previous: Particle Effects](@previous) | Page 10 | [Next: Create a Holiday Card!](@next)
  
